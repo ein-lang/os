36c0e7eb411cc552f5d9c34d9240344c01f233a9
Yota Toyama <raviqqe@gmail.com>
2021-01-29 02:53:58 +0000

    Remove unused code

--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -3,7 +3,6 @@ use std::fs::File;
 use std::io::Write;
 use std::os::raw::{c_int, c_void};
 use std::os::unix::io::FromRawFd;
-use std::process::exit;
 
 extern "C" {
     static _ein_system_main:
@@ -25,9 +24,4 @@ extern "C" fn _ein_system_fd_write(fd: ffi::Number, buffer: ffi::EinString) -> f
     let mut file = unsafe { File::from_raw_fd(f64::from(fd) as i32) };
 
     (file.write(buffer.as_slice()).unwrap() as f64).into()
-}
-
-#[no_mangle]
-extern "C" fn _ein_system_exit(code: ffi::Number) -> ffi::None {
-    exit(f64::from(code) as i32)
 }
