e15b6494f07b07fd394b9797acf36f1089c4a1eb
Yota Toyama <raviqqe@gmail.com>
2021-01-31 17:31:35 -0800

    fdRead (#7)
    * Implement fdRead
    
    * Add more file descriptors
    
    * Export more variables
    
    * Fix buffer

--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -4,7 +4,7 @@ use bdwgc_alloc::Allocator;
 use result::FfiResult;
 use std::alloc::Layout;
 use std::fs::File;
-use std::io::Write;
+use std::io::{Read, Write};
 use std::os::raw::{c_int, c_void};
 use std::os::unix::io::FromRawFd;
 
@@ -25,6 +25,25 @@ pub extern "C" fn main() -> c_int {
     unsafe { Allocator::initialize() }
 
     f64::from(unsafe { _ein_system_main(ffi::None::new()) }) as c_int
+}
+
+#[no_mangle]
+extern "C" fn _ein_system_fd_read(
+    fd: ffi::Number,
+    buffer_size: ffi::Number,
+) -> *const FfiResult<ffi::EinString> {
+    let mut file = unsafe { File::from_raw_fd(f64::from(fd) as i32) };
+    let mut buffer = vec![0; f64::from(buffer_size) as usize];
+
+    let count = match file.read(&mut buffer) {
+        Ok(count) => count,
+        Err(error) => return FfiResult::from_io_error(error),
+    };
+    buffer.resize(count, 0);
+
+    std::mem::forget(file);
+
+    FfiResult::ok(ffi::EinString::from(buffer))
 }
 
 #[no_mangle]
