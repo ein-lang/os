7d69bf67548cb8893e688dd645f5e02aace6444e
Yota Toyama <raviqqe@gmail.com>
2021-01-31 16:30:38 -0800

    Return errors from APIs (#6)
    * Return errors
    
    * Wrap fdWrite

---
+++ ffi/src/result.rs
@@ -0,0 +1,31 @@
+#[repr(C)]
+pub struct FfiResult<T: Default> {
+    value: T,
+    error: ffi::Number,
+}
+
+impl<T: Default> FfiResult<T> {
+    pub fn ok(value: T) -> *const Self {
+        Self {
+            value,
+            error: (-1.0).into(),
+        }
+        .leak()
+    }
+
+    pub fn error(error: impl Into<ffi::Number>) -> *const Self {
+        Self {
+            value: Default::default(),
+            error: error.into(),
+        }
+        .leak()
+    }
+
+    pub fn from_io_error(error: std::io::Error) -> *const Self {
+        Self::error(error.raw_os_error().map(f64::from).unwrap_or(std::f64::NAN))
+    }
+
+    fn leak(self) -> *const Self {
+        Box::leak(Box::new(self))
+    }
+}
