9cba1518d7c2a0ba3e49d1061a012dccdc7c4bcf
Yota Toyama <raviqqe@gmail.com>
2021-03-03 00:20:22 -0800

    realloc implementation (#8)
    * Implement _ein_realloc
    
    * Update bdwgc-alloc
    
    * Update comment
    
    * Refactor

--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -12,12 +12,27 @@ extern "C" {
     fn _ein_system_main(argument: ffi::None) -> ffi::Number;
 }
 
+const DEFAULT_ALIGNMENT: usize = 8;
+
 #[global_allocator]
 static GLOBAL_ALLOCATOR: Allocator = Allocator;
 
 #[no_mangle]
 pub extern "C" fn _ein_malloc(size: usize) -> *mut c_void {
-    (unsafe { std::alloc::alloc(Layout::from_size_align(size, 8).unwrap()) }) as *mut c_void
+    (unsafe { std::alloc::alloc(Layout::from_size_align(size, DEFAULT_ALIGNMENT).unwrap()) })
+        as *mut c_void
+}
+
+#[no_mangle]
+pub extern "C" fn _ein_realloc(pointer: *mut c_void, size: usize) -> *mut c_void {
+    // Layouts are ignored by the bdwgc global allocator.
+    (unsafe {
+        std::alloc::realloc(
+            pointer as *mut u8,
+            Layout::from_size_align(0, DEFAULT_ALIGNMENT).unwrap(),
+            size,
+        )
+    }) as *mut c_void
 }
 
 #[no_mangle]
