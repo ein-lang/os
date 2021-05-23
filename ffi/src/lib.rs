6db476811b9cbed676e3a03a39cdc319d513b420
Yota Toyama <raviqqe@gmail.com>
2021-05-23 21:17:43 +0000

    Remove bdwgc

--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -1,6 +1,5 @@
 mod result;
 
-use bdwgc_alloc::Allocator;
 use result::FfiResult;
 use std::alloc::Layout;
 use std::fs::File;
@@ -19,9 +18,6 @@ extern "C" {
 }
 
 const DEFAULT_ALIGNMENT: usize = 8;
-
-#[global_allocator]
-static GLOBAL_ALLOCATOR: Allocator = Allocator;
 
 #[no_mangle]
 pub extern "C" fn _ein_malloc(size: usize) -> *mut c_void {
@@ -43,8 +39,6 @@ pub extern "C" fn _ein_realloc(pointer: *mut c_void, size: usize) -> *mut c_void
 
 #[no_mangle]
 pub extern "C" fn main() -> c_int {
-    unsafe { Allocator::initialize() }
-
     let mut stack = ffi::cps::Stack::new(INITIAL_STACK_CAPACITY);
 
     unsafe { _ein_os_main(&mut stack, exit, ffi::None::new()) };
@@ -60,34 +54,34 @@ extern "C" fn exit(_: *mut ffi::cps::Stack, code: f64) -> ffi::cps::Result {
 extern "C" fn _ein_os_fd_read(
     fd: ffi::Number,
     buffer_size: ffi::Number,
-) -> *const FfiResult<ffi::EinString> {
+) -> ffi::Arc<FfiResult<ffi::EinString>> {
     let mut file = unsafe { File::from_raw_fd(f64::from(fd) as i32) };
     let mut buffer = vec![0; f64::from(buffer_size) as usize];
 
     let count = match file.read(&mut buffer) {
         Ok(count) => count,
-        Err(error) => return FfiResult::from_io_error(error),
+        Err(error) => return ffi::Arc::new(error.into()),
     };
     buffer.resize(count, 0);
 
     std::mem::forget(file);
 
-    FfiResult::ok(ffi::EinString::from(buffer))
+    FfiResult::ok(ffi::EinString::from(buffer)).into()
 }
 
 #[no_mangle]
 extern "C" fn _ein_os_fd_write(
     fd: ffi::Number,
     buffer: ffi::EinString,
-) -> *const FfiResult<ffi::Number> {
+) -> ffi::Arc<FfiResult<ffi::Number>> {
     let mut file = unsafe { File::from_raw_fd(f64::from(fd) as i32) };
 
     let byte_count = match file.write(buffer.as_slice()) {
         Ok(count) => count,
-        Err(error) => return FfiResult::from_io_error(error),
+        Err(error) => return ffi::Arc::new(error.into()),
     };
 
     std::mem::forget(file);
 
-    FfiResult::ok((byte_count as f64).into())
+    ffi::Arc::new(FfiResult::ok((byte_count as f64).into()))
 }
